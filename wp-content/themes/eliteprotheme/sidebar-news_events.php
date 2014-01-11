f897d811d945e705dcfc3686865ea238f0a04641
danepliz <danepliz@gmail.com>
2014-01-11 18:29:49 +0545

    designed changed

--- wp-content/themes/eliteprotheme/sidebar-news_events.php
+++ wp-content/themes/eliteprotheme/sidebar-news_events.php
@@ -1,24 +1,42 @@
-<div class="grid_sub_custom sub_alpha box_type" >
-  <div class="footer_widget">
+<div class="grid_sub_custom_home sub_alpha box_type box_type_custom widget_with_list" >
     <?php if (is_active_sidebar('first-footer-widget-area')) : ?>
     <?php dynamic_sidebar('first-footer-widget-area'); ?>
     <?php else : ?>
     <h4>Upcoming Events</h4>
-    <hr/>
-    <p>All upcoming Events here</p>
+    	<ul>
+    		<?php for($i = 1; $i < 4; $i++):?>
+				<li>
+					<img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-'.$i.'.jpg' ?>" />	
+					<p>
+						Lorem ipsum dolar signo leytmis lura. Vengaro testaro eventure glyarina 20 menasum
+					</p>
+					<a href="#" class="readmore">read more</a>			
+				</li>    
+	    	<?php endfor; ?>
+    	</ul>
+    	
+    	<a href="#" class="viewall">view all</a>
     <?php endif; ?>
-  </div>
 </div>
-<div class="grid_sub_custom sub_omega box_type">
-  <div class="footer_widget ">
+
+
+<div class="grid_sub_custom_home sub_alpha box_type box_type_custom widget_with_list">
     <?php if (is_active_sidebar('second-footer-widget-area')) : ?>
     <?php dynamic_sidebar('secpnd-footer-widget-area'); ?>
     <?php else : ?>
-    <h4>Latest News</h4>
-    <hr/>
-    <p>All the latest News here</p>
-    
+    <h4 class="wd-h4">Latest News</h4>
+    	<ul>
+    		<?php for($i = 1; $i < 4; $i++):?>
+				<li>
+					<img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-'.$i.'.jpg' ?>" />	
+					<p>
+						Lorem ipsum dolar signo leytmis lura. Vengaro testaro eventure glyarina 20 menasum
+					</p>
+					<a href="#" class="readmore">read more</a>			
+				</li>    
+	    	<?php endfor; ?>
+    	</ul>
+    	<a href="#" class="viewall">view all</a>
     <?php endif; ?>
-  </div>
 </div>
 
--- wp-content/themes/eliteprotheme/sidebar-videoslider.php
+++ wp-content/themes/eliteprotheme/sidebar-videoslider.php
@@ -1,12 +1,15 @@
 
-<div class="grid_sub_videoslider sub_alpha box_type" >
-  <div class="footer_widget">
+<div class="grid_sub_videoslider sub_alpha box_type_custom" >
     <?php if (is_active_sidebar('video-footer-widget-area')) : ?>
     <?php dynamic_sidebar('video-footer-widget-area'); ?>
     <?php else : ?>
-    <h4>Latest Videos</h4>
-    <hr/>
+    <h4 class="bglg">Latest Videos</h4>
+    <ul>
+    	<li><img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-1.jpg' ?>" /></li>
+    	<li><img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-2.jpg' ?>" /></li>
+    	<li><img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-3.jpg' ?>" /></li>
+    	<li><img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-1.jpg' ?>" /></li>
+    </ul>
     <?php endif; ?>
-  </div>
 </div>
 
