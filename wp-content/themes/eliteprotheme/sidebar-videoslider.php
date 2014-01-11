8548deb6bbba0d5b583245561c85eaff5a4abb4e
danepliz <danepliz@gmail.com>
2014-01-12 00:01:21 +0545

    design changed

--- wp-content/themes/eliteprotheme/sidebar-videoslider.php
+++ wp-content/themes/eliteprotheme/sidebar-videoslider.php
@@ -4,12 +4,12 @@
     <?php dynamic_sidebar('video-footer-widget-area'); ?>
     <?php else : ?>
     <h4 class="bglg">Latest Videos</h4>
-    <ul>
-    	<li><img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-1.jpg' ?>" /></li>
-    	<li><img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-2.jpg' ?>" /></li>
-    	<li><img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-3.jpg' ?>" /></li>
-    	<li><img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-1.jpg' ?>" /></li>
-    </ul>
+<!--     <ul>
+	    	<li><img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-1.jpg' ?>" /></li>
+	    	<li><img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-2.jpg' ?>" /></li>
+	    	<li><img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-3.jpg' ?>" /></li>
+	    	<li><img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-1.jpg' ?>" /></li>
+		</ul> -->
     <?php endif; ?>
 </div>
 
