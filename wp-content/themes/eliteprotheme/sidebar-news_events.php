581c1aa25765b1b2fa502f9ba6d9e7c3a408c886
danepliz <danepliz@gmail.com>
2014-01-16 20:28:16 +0545

    changed style NOTE : use default style from theme style option

--- wp-content/themes/eliteprotheme/sidebar-news_events.php
+++ wp-content/themes/eliteprotheme/sidebar-news_events.php
@@ -1,21 +1,13 @@
 <div class="grid_sub_custom_home sub_alpha box_type box_type_custom widget_with_list" >
     <?php if (is_active_sidebar('first-footer-widget-area')) : ?>
     <?php dynamic_sidebar('first-footer-widget-area'); ?>
+    <a href="#" class="viewall">view all</a>
     <?php else : ?>
-    <h4>Upcoming Events</h4>
-    	<ul>
-    		<?php for($i = 1; $i < 4; $i++):?>
-				<li>
-					<img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-'.$i.'.jpg' ?>" />	
-					<p>
-						Lorem ipsum dolar signo leytmis lura. Vengaro testaro eventure glyarina 20 menasum
+    <h4>Latest News</h4>
+    	<p>
+						No News added
 					</p>
-					<a href="#" class="readmore">read more</a>			
-				</li>    
-	    	<?php endfor; ?>
-    	</ul>
     	
-    	<a href="#" class="viewall">view all</a>
     <?php endif; ?>
 </div>
 
@@ -24,19 +16,10 @@
     <?php if (is_active_sidebar('second-footer-widget-area')) : ?>
     <?php dynamic_sidebar('secpnd-footer-widget-area'); ?>
     <?php else : ?>
-    <h4 class="wd-h4">Latest News</h4>
-    	<ul>
-    		<?php for($i = 1; $i < 4; $i++):?>
-				<li>
-					<img src="<?php echo site_url().'/wp-content/themes/eliteprotheme/images/img-'.$i.'.jpg' ?>" />	
+    <h4 class="wd-h4">Latest Events</h4>
 					<p>
-						Lorem ipsum dolar signo leytmis lura. Vengaro testaro eventure glyarina 20 menasum
+						No Events added
 					</p>
-					<a href="#" class="readmore">read more</a>			
-				</li>    
-	    	<?php endfor; ?>
-    	</ul>
-    	<a href="#" class="viewall">view all</a>
     <?php endif; ?>
 </div>
 
